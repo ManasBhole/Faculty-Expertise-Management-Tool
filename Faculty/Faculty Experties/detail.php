772a52ee49211498fcabf58387c87ac6edaf202a
FABULOUSSAM <shubham.mohapess@gmail.com>
2020-06-17 18:54:15 +0530

    First commit

---
+++ Faculty/Faculty Experties/detail.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="detailstyle.css">
+    <!-- CSS only -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <title>Detail</title>
+</head>
+<header>
+<body>
+<div class="container">
+    <div class="box">
+        <div class="content">
+            <h3>Faculty as Resource Person</h3>
+            <a href="option1.php">Read More</a>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="box">
+        <div class="content">
+            <h3>Competitive Exams</h3>
+            <a href="option2.php">Read More</a>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="box">
+        <div class="content">
+            <h3>Qualification till-date</h3>
+            <a href="#">Read More</a>
+        </div>
+    </div>
+</div>
+</header>
+<section>
+<div class="container">
+    <div class="box">
+        <div class="content">
+            <h3>Awards</h3>
+            <a href="#">Read More</a>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="box">
+        <div class="content">
+            <h3>Faculty Promotion</h3>
+            <a href="#">Read More</a>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="box">
+        <div class="content">
+            <h3>Faculty Long Live</h3>
+            <a href="#">Read More</a>
+        </div>
+    </div>
+</div>
+</section>
+</body>
+</html>
---
+++ Faculty/Faculty Experties/option1.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <link rel="stylesheet" type="text/css" href="optionstyle.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  
+    <title>OPTION1</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-dark ">
+    <div class="navbar_title">Faculty as Resource Person</div>
+<img src="../img/rait logo.jpeg" class="navbar-brand ml-auto " alt="#" width=160px>
+</nav>
+
+<form action="/action_page.php">
+  <div class="form-group">
+    <label for="Name">Name:</label>
+    <div class="col-5"><input type="name" class="form-control border border-secondary" placeholder="Enter name" id="name"></div>
+  
+</div>
+  <div class="form-group">
+  <label for="sel1">Select list:</label>
+  <div class="col-4">
+  <select class="form-control border border-secondary" id="sel1">
+    <option>Expert speaker</option>
+    <option>Judge</option>
+    <option>Chairperson</option>
+    <option>Reviewer (for paper orPh.D. related)</option>
+    <option>Lectures in Industry</option>
+  </select>
+  </div>
+</div>
+  <div class="form-group">
+    <label for="topic">Topic:</label>
+    <div class="col-4">
+    <input type="topic" class="form-control border border-secondary" placeholder="Topic name" id="tpc">
+</div>
+    </div>
+  <div class="form-group">
+    <label for="Event name">Event/Course Name:</label>
+    <div class="col-4">
+    <input type="name" class="form-control border border-secondary" placeholder="Event Name/Course Name" id="event">
+    </div>
+  </div>
+  <div class="form-group">
+  <label for="sel1">Select level:</label>
+  <div class="col-4">
+  <select class="form-control border border-secondary" id="lvl">
+    <option>Local level</option>
+    <option>State level</option>
+    <option>National level</option>
+    <option>International level</option>
+    
+  </select>
+  </div>
+  
+    </div>
+    <div class="form-group">
+    <label for="Event name">Venue</label>
+    <div class="col-5"><input type="name" class="form-control border border-secondary" placeholder="venue name" id="venue"></div>
+   
+    </div>
+    <div class="col-5">
+    <label for="example-date-input" >Date</label>
+     <div class="col-6">
+    <input class="form-control border border-secondary" type="date" value="2011-08-19" id="example-date-input">
+    </div>
+     
+  </div>
+  <div class="btn">
+  <button type="submit" class="btn btn-primary ">Submit</button>
+    </div>
+</form>
+
+    
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+ 
+</body>
+</html>
