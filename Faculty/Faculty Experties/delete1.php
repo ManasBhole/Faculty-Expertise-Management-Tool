a11073c90846f55f4cd9544ca2ce23608d2cdb85
SmrutiSingh12 <66952472+SmrutiSingh12@users.noreply.github.com>
2020-06-29 19:22:03 +0530

    delete1 added

---
+++ Faculty/Faculty Experties/delete1.php
@@ -0,0 +1,8 @@
+<?php
+@session_start();
+include('connect.php');
+$id=$_REQUEST['id'];
+$query = "DELETE FROM awards WHERE id=$id"; 
+$result = mysqli_query($conn,$query) or die ( mysqli_error());
+header("Location: detail.php"); 
+?>
