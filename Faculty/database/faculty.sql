2fa3dd5e0853a6ff2936fe2523d9576747942607
FABULOUSSAM <shubham.mohapess@gmail.com>
2020-08-01 19:41:24 +0530

    Database updated

--- Faculty/database/faculty.sql
+++ Faculty/database/faculty.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 21, 2020 at 06:11 AM
+-- Generation Time: Aug 01, 2020 at 04:10 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -25,6 +25,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin_detail`
+--
+
+CREATE TABLE `admin_detail` (
+  `Username` varchar(200) NOT NULL,
+  `Password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin_detail`
+--
+
+INSERT INTO `admin_detail` (`Username`, `Password`) VALUES
+('admin', '12345'),
+('admin', '12345');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `awards`
 --
 
@@ -41,6 +60,13 @@ CREATE TABLE `awards` (
   `pdf` varchar(200) DEFAULT NULL,
   `Sdrn` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `awards`
+--
+
+INSERT INTO `awards` (`id`, `Faculty_name`, `Award_name`, `Position`, `Event_name`, `Date`, `University`, `College_name`, `Level`, `pdf`, `Sdrn`) VALUES
+(1, 'Shubham Ashok Mohape', 'Best Student', '1st', 'award distribution', '2020-07-10', 'mumbai', 'beststudent', 'State level', 'upload_files/awards/721-Student Application Form PART-1.pdf', 12345);
 
 -- --------------------------------------------------------
 
@@ -60,6 +86,13 @@ CREATE TABLE `competitive_exam` (
   `pdf` varchar(200) DEFAULT NULL,
   `Sdrn` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `competitive_exam`
+--
+
+INSERT INTO `competitive_exam` (`id`, `Faculty_name`, `PET_appeared`, `PET_date`, `PET_score`, `GATE_appeared`, `GATE_date`, `GATE_score`, `pdf`, `Sdrn`) VALUES
+(1, 'Shubham Ashok Mohape', '12', '2020-07-10', 121, '1', '2020-07-30', 123, 'upload_files/exams/276-Preet_Project Proposal Submission Format.pdf', 12345);
 
 -- --------------------------------------------------------
 
@@ -85,7 +118,8 @@ CREATE TABLE `faculty_as_resource` (
 --
 
 INSERT INTO `faculty_as_resource` (`id`, `Faculty_name`, `Resource_person`, `Topic_name`, `Event_name`, `Level`, `Venue`, `Date`, `pdf`, `Sdrn`) VALUES
-(1, 'Shubham Ashok Mohape', 'Lectures in Industry', 'none', 'none', 'Local level', 'rait', '2020-07-21', '', 12345);
+(1, 'Shubham Ashok Mohape', 'Lectures in Industry', 'none', 'none', 'Local level', 'rait', '2020-07-21', '', 12345),
+(2, 'Shubham Ashok Mohape', 'Chairperson', 'NONE', 'NONE', 'International level', 'RAIT', '2020-07-28', 'upload_files/resource_person/662-Preet_Project Proposal Submission Format.pdf', 12345);
 
 -- --------------------------------------------------------
 
@@ -115,7 +149,8 @@ CREATE TABLE `faculty_info` (
 --
 
 INSERT INTO `faculty_info` (`Sdrn`, `First_name`, `Middle_name`, `Last_name`, `DOB`, `Department`, `Contact_no`, `Addr`, `Email`, `Doj`, `Qualification`, `Desig`, `Password`, `OTP`) VALUES
-('12345', 'Shubham', 'Ashok', 'Mohape', '2020-07-19', 'Comps', 1234567899, 'none', 'shubham.mohapess@gmail.com', '2020-07-20', 'none', 'none', '12345', NULL);
+('12345', 'Shubham', 'Ashok', 'Mohape', '2020-07-19', 'Comps', 1234567899, 'none', 'shubham.mohapess@gmail.com', '2020-07-20', 'none', 'none', '12345', NULL),
+('007', 'chirag', 'rajesh', 'nawale', '0000-00-00', '', 0, '', 'shubham.mohapess@gmail.com', '0000-00-00', '', '', '12345', NULL);
 
 -- --------------------------------------------------------
 
@@ -133,6 +168,14 @@ CREATE TABLE `faculty_long_live` (
   `pdf` varchar(200) DEFAULT NULL,
   `Sdrn` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `faculty_long_live`
+--
+
+INSERT INTO `faculty_long_live` (`id`, `Faculty_name`, `Reason_long_live`, `From_date`, `To_date`, `Date_of_joining_after_long_live`, `pdf`, `Sdrn`) VALUES
+(1, 'Shubham Ashok Mohape', 'nothing', '2020-07-09', '2020-07-30', '2020-07-31', '', 12345),
+(2, 'Shubham Ashok Mohape', '', '0000-00-00', '0000-00-00', '0000-00-00', 'upload_files/long_live/303-vlab.pdf', 12345);
 
 -- --------------------------------------------------------
 
@@ -158,6 +201,13 @@ CREATE TABLE `faculty_promotion` (
   `Sdrn` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `faculty_promotion`
+--
+
+INSERT INTO `faculty_promotion` (`id`, `Faculty_name`, `Date_of_joining`, `SDNR_number`, `RAIT_experience`, `Other_experience`, `Industry_experience`, `Total_experience`, `Starting_designation`, `Promotion_1`, `Date_promotion_1`, `Promotion_2`, `Date_promotion_2`, `pdf`, `Sdrn`) VALUES
+(1, 'Shubham Ashok Mohape', '2020-07-16', '12345', '02y-06m', '03y-02m', '05y-05m', '05y-05m', 'none', 'none', '2020-07-29', 'none', '2020-07-30', '', 12345);
+
 -- --------------------------------------------------------
 
 --
@@ -179,6 +229,13 @@ CREATE TABLE `qualification` (
   `pdf` varchar(200) DEFAULT 'n/a',
   `Sdrn` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `qualification`
+--
+
+INSERT INTO `qualification` (`id`, `Faculty_name`, `Admitted_for_program`, `Specialization`, `Year_of_admission`, `University`, `Registration_number`, `College_name`, `Status`, `Research_topic`, `Guide_name`, `pdf`, `Sdrn`) VALUES
+(1, 'Shubham Ashok Mohape', 'none', 'none', 2000, 'none', 1234, 'none', 'none', 'none', 'none', 'upload_files/qualification/194-SDG RAIT Summer Internship Program  2020-Student Allocation Final.pdf', 12345);
 
 --
 -- Indexes for dumped tables
@@ -228,37 +285,37 @@ ALTER TABLE `qualification`
 -- AUTO_INCREMENT for table `awards`
 --
 ALTER TABLE `awards`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `competitive_exam`
 --
 ALTER TABLE `competitive_exam`
-  MODIFY `id` int(26) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(26) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `faculty_as_resource`
 --
 ALTER TABLE `faculty_as_resource`
-  MODIFY `id` int(26) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(26) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `faculty_long_live`
 --
 ALTER TABLE `faculty_long_live`
-  MODIFY `id` int(26) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(26) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `faculty_promotion`
 --
 ALTER TABLE `faculty_promotion`
-  MODIFY `id` int(26) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(26) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `qualification`
 --
 ALTER TABLE `qualification`
-  MODIFY `id` int(26) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(26) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
